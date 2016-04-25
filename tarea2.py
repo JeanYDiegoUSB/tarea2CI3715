4212571afa9ac8a34f862f931977f60ac78dee34
JeanYDiegoUSB <12-10848@usb.ve>
2016-04-24 23:55:06 -0430

    Added files via upload
    Lo pude resolver. Mañana (Lunes 2016-4-25) revisamos con cual solución nos quedamos.

--- tarea2.py
+++ tarea2.py
@@ -1,13 +1,11 @@
 '''
 Created on Apr 22, 2016
-
 @author:
         Jean Paul Alexander Lacour
-        Diego Daniel Pedroza Pérez
+        Diego Daniel Pedroza Perez
 '''
-
+from datetime import datetime
 import math
-from datetime import datetime 
 
 '''
     Clase Tarifa
@@ -25,6 +23,14 @@ class Tarifa(object) :
     #metodo para obtener el valor del campo fin
     def getFin(self) :
         return self.fin
+    
+    #metodo para ver que tarifa usar
+    def cualTarifa(self, fecha) :
+        if fecha.weekday() in [0,1,2,3,4] :
+            return self.getSemana()
+        elif fecha.weekday() in [5,6] :
+            return self.getFin()
+    
 '''
     Metodo para saber si un anio es bisiesto
 '''
@@ -50,85 +56,18 @@ def maxDay(month, year) :
             return 28
 
 '''
-    Funcion para saber cuantas horas son de tipo semana
-    y cuantas son de tipo fin de semana. Recibe 
-    un tiempoInicial y un tiempFinal de tipo datetime
-    y devuelve un arreglo de enteros [horaSemana,horaFinSemana]
-'''
-def tipoHoraMesIgual(inicio,fin):
-    arreglo = [0,0]            #Creando el arreglo [horaSemana,horaFinSemana]
-    
-    #Si los dias no son iguales
-    if inicio.day!=fin.day :
-        #Verifico que dia(si es fin de semana o no) y guardo la cantidad de horas en el espacio 
-        #correspondiente
-        x=inicio.weekday();
-        if(x<=4):     
-            arreglo[0]=24-math.ceil(inicio.hour+(inicio.minute)/100)
-        else:
-            arreglo[1]=24-math.ceil(inicio.hour+(inicio.minute)/100)
-
-        #Veo que dia de la semana es dia siguiente del inicio
-        if x == 6 :
-            x = 0
-        else:
-            x +=1
-        b=1
-        totalDias = fin.day-inicio.day
-
-        # Mientras no he revisado todos los dias que hay entre el dia
-        #siguiente del inicio y el final, voy agregando las horas
-        #de los días, teniendo en cuenta el tipo de día que es.   
-        while(b < totalDias):
-            if x<5 :
-                arreglo[0]+=24
-                x+=1
-            else:
-                arreglo[1]+=24
-                if x==6 :
-                    x=0
-                else:
-                    x+=1
-            b+=1
-
-        #
-        if(fin.weekday()<5):
-            arreglo[0]+=math.ceil(fin.hour+(fin.minute/100))
-        else:
-            arreglo[1]+=math.ceil(fin.hour+(fin.minute/100))
-
-
-    #Si los dias son iguales
-    else:
-        horasTotal=fin.hour-inicio.hour
-        #Se cobra por hora completa
-        if(fin.minute!=0 or inicio.minute!=0):
-            if(inicio.minute!=0):
-                horasTotal+=1
-            if(fin.minute!=0):
-                horasTotal+=1
-
-        if(inicio.weekday()<5):
-            arreglo[0]=horasTotal
-        else:
-            arreglo[1]=horasTotal
-
-
-    return arreglo
-
-'''
     Metodo para calcular el monto a pagar por un trabajo
     dado un objeto Tarifa y un intervalo tiempoDeTrabajo
 '''
 def calcularPrecio(tarifa, tiempoDeTrabajo) :
     inicio,fin = tiempoDeTrabajo[0],tiempoDeTrabajo[1] # fechas de inicio y fin del trabajo
-
+    monto = 0
     #Si los anios, meses y dias son iguales
     if inicio.year == fin.year and inicio.month == fin.month and inicio.day == fin.day :
         #diferencia entre los horarios
         dif = fin.hour - inicio.hour
         if dif >= 0.25 :
-            return tarifa.getSemana() * math.ceil(dif)
+            return tarifa.cualTarifa(inicio) * math.ceil(dif)
     #Si los anios y meses son iguales
     elif inicio.year == fin.year and inicio.month == fin.month :
         #diferencia entre los dias, se suma la hora en que termina el trabajo
@@ -136,7 +75,14 @@ def calcularPrecio(tarifa, tiempoDeTrabajo) :
         dif = math.ceil(24 * (fin.day - inicio.day) - inicio.hour + fin.hour)
         #verificar que no se pasa del limite establecido
         if dif <= 168 :
-            return tarifa.getSemana() * dif
+            monto += (24 - inicio.hour) * tarifa.cualTarifa(inicio)
+            d = inicio.day + 1
+            while d != fin.day :
+                aux = datetime(inicio.year,inicio.month,d)
+                monto += 24 * tarifa.cualTarifa(aux)
+                d += 1
+            monto += fin.hour * tarifa.cualTarifa(fin)
+            return monto
     #Si los anios son iguales
     elif inicio.year == fin.year :
         #Si son meses seguidos
@@ -148,7 +94,18 @@ def calcularPrecio(tarifa, tiempoDeTrabajo) :
                 dif = math.ceil(24 * dias - inicio.hour + fin.hour)
                 #verificar que no se pasa del limite establecido
                 if dif <= 168 :
-                    return tarifa.getSemana() * dif
+                    monto += (24 - inicio.hour) * tarifa.cualTarifa(inicio)
+                    d = inicio.day + 1
+                    m = inicio.month
+                    while m != fin.month or d != fin.day :
+                        aux = datetime(inicio.year,m,d)
+                        monto += 24 * tarifa.cualTarifa(aux)
+                        if d == maxDay(inicio.month, inicio.year) :
+                            m = fin.month
+                            d = 0
+                        d += 1
+                    monto += fin.hour * tarifa.cualTarifa(fin)
+                    return monto
     #Si los anios son diferentes
     elif inicio.year != fin.year :
         #Si el mes de inicio es diciembre y el mes de fin es enero
@@ -160,37 +117,17 @@ def calcularPrecio(tarifa, tiempoDeTrabajo) :
                 dif = math.ceil(24 * dias - inicio.hour + fin.hour)
                 #verificar que no se pasa del limite establecido
                 if dif <= 168 :
-                    return tarifa.getSemana() * dif
-    #Si no trabaja lo suficiente o trabaja de más
+                    monto += (24 - inicio.hour) * tarifa.cualTarifa(inicio)
+                    d = inicio.day + 1
+                    m = inicio.month
+                    while m != fin.month or d != fin.day :
+                        aux = datetime(inicio.year,m,d)
+                        monto += 24 * tarifa.cualTarifa(aux)
+                        if d == maxDay(inicio.month, inicio.year) :
+                            m = fin.month
+                            d = 0
+                        d += 1
+                    monto += fin.hour * tarifa.cualTarifa(fin)
+                    return monto
+    #Si no trabaja lo suficiente o trabaja de mas
     return 0
-
-
-
-if __name__ == "__main__":
-    sem = int(input("Introduzca tarifa de la semana: "))
-    finSem = int(input("Introduzca tarifa del fin de semana: "))
-    tiempInit= str(input(
-        "Introduzca tiempoDeTrabajo Inicial\nSepare mediante comas\n year,month,day,hour,minutes:  "
-    ))
-    tiempFin= str(input(
-        "Introduzca tiempoDeTrabajo Final\nSepare mediante comas\nyear,month,day,hour,minutes:  "
-    ))
-    a= tiempInit.split(",")
-    b =tiempFin.split(",")
-    try:
-        tiempoWorkInit=datetime(int(a[0]),int(a[1]),int(a[2]),int(a[3]), int(a[4]), 0, 0,None)
-    except (TypeError,IndexError,ValueError):
-        print("Error! de Tipo")
-    try:
-        tiempoWorkFin= datetime(int(b[0]),int(b[1]),int(b[2]),int(b[3]), int(b[4]), 0, 0,None)
-    except (TypeError,IndexError,ValueError):
-        print("Error! de Tipo")
-    tarifa = Tarifa(sem,finSem);
-    tiempoDeTrabajo=[tiempoWorkInit,tiempoWorkFin]
-    
-    c=tipoHoraMesIgual(tiempoWorkInit,tiempoWorkFin)
-    print("Horas_Sem: "+str(c[0])+" Horas Fin: "+str(c[1]))
-   
-    print("SALIENDO")
-    
-
